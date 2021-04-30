6150b680875510c457952f4b5a6c055a8d53abe7
martin <martinamezola2000@gmail.com>
2021-04-30 16:43:54 +0200

    cambios en main

--- main.sh
+++ main.sh
@@ -9,25 +9,36 @@
 #										#
 #########################################
 
+#Beste fitxategietatik funztzioak inportatu
+. ./funtzioak/desinstalazioa.sh
+. ./funtzioak/apacheInstalatu.sh
+. ./funtzioak/apacheAbiarazi.sh
+. ./funtzioak/apache80PortuaBegiratu.sh
+. ./funtzioak/irekiIndex.sh
+. ./funtzioak/sortuWebPertsonala.sh
+. ./funtzioak/sortuWebOstatu.sh
+. ./funtzioak/apache8080PortuaBegiratu.sh
+. ./funtzioak/PHPInstalatu.sh
+. ./funtzioak/PHPTestatu.sh
+. ./funtzioak/phpBB3Instalatu.sh
+. ./funtzioak/PythonInstalatu.sh
+. ./funtzioak/searxHasieratu.sh
+. ./funtzioak/searxInstalatu.sh
+. ./funtzioak/searxAbiarazi.sh
+. ./funtzioak/urlAtzituenak.sh
 
-#Importar funciones de otros ficheros
-#. ./funciones/realizarCopiaAgenda.sh
-#. ./funciones/vaciarAgenda.sh
-#. ./funciones/despedirse.sh
-#. ./funciones/pedirAnadirContacto.sh
-#. ./funciones/eliminar.sh
-#. ./funciones/existe.sh
-#. ./funciones/modificarContacto.sh
-#. ./funciones/verTodosContactos.sh
-#. ./funciones/mostrarContacto.sh
+
+
+
+
 
 #Konstanteen definizioa
-#FICH_AUK_MENU="temp/aukera.txt"
+FITX_AUK_MENU="temp/aukera.txt"
 
-### Comienzo del programa ###
-opcionMenuPpal=0
+### Programaren Hasiera ###
+menuAukerapPal=0
 
-while test $opcionMenuPpal -ne 17
+while test $menuAukerapPal -ne 17
 do
 	#Menua bistaratu
 	dialog --backtitle "Web Aplikazioen Instalazioa 2021 - SEO" --title "Web Aplikazioen Instalazioa" \
@@ -35,11 +46,11 @@ do
 			1 "Desinstalatu eta ezabatu dena."\
 			2 "Apache instalatu."\
 			3 "APache abiarazi."\
-			4 "Apache-ren portua begiratu."\
+			4 "Apache-ren portua begiratu. (80 portua)"\
 			5 "Ireki index.html"\
 			6 "Sortu web pertsonala."\
 			7 "Web-ostatu birtuala sortu."\
-			8 "Apache-ren portua begiratu."\
+			8 "Apache-ren portua begiratu. (8080 portua)"\
 			9 "PHP instalatu."\
 			10 "PHP testatu"\
 			11 "phpBB3 instalatu"\
@@ -49,33 +60,41 @@ do
 			15 "searx abiarazi"\
 			16 "Apache-n hamar URL atzituenak"\
 			17 "Irten. :)" \
-			2> $FICH_AUK_MENU
+			2> $FITX_AUK_MENU
 
 	if test $? -eq 0
 	then
-		#Extraer opcion marcada por el usuario
-		opcionMenuPpal=`more $FICH_AUK_MENU`
+		#Erabiltzaileak aukeratutako aukera atera
+		menuAukerapPal=`more $FITX_AUK_MENU`
 
-		#Borrar el fichero temporal con la respuesta del usuario
-		rm $FICH_AUK_MENU
+		#Fitxategi temporala (Erabiltzailearen erantzuna gordetzen duena) ezabatu.
+		rm $FITX_AUK_MENU
 
-		#Realizar accion especifica en base a la eleccion del usuario
-		case $opcionMenuPpal in
-			1) pedirContacto;;
-			2) eliminarContactoUsuario;;
-			3) mostrarContacto;;
-			4) modificarContacto;;
-			5) realizarCopia;;
-			6) vaciarAgenda;;
-			7) verTodosContactos;;
-			8) despedirse;;
+		#Erabiltzaileak aukeratutakoaren arabera funtzio konkretu bat egin.
+		case $menuAukerapPal in
+			1) desinstalazioa ;; # Desinstalatu eta ezabatu dena.
+			2) apacheInstalatu;;
+			3) apacheAbiarazi;;
+			4) apache80PortuaBegiratu;;
+			5) irekiIndex;;
+			6) sortuWebPertsonala;;
+			7) sortuWebOstatu;;
+			8) apache8080PortuaBegiratu;;
+			9) PHPInstalatu ;;
+			10) PHPTestatu ;;
+			11) phpBB3Instalatu ;;
+			12) PythonInstalatu ;;
+			13) searxHasieratu ;; # searx erabiltzailea eta ingurune birtuala sortu
+			14) searxInstalatu ;;
+			15) searxAbiarazi ;;
+			16) urlAtzituenak ;; # Apache-n hamar URL atzituenak
 			*) ;;
-		esac #Fin seleccion de accion especifica del usuario
+		esac #Aukeratutako futzio konkretuaren amaiera.
 	else
-		opcionMenuPpal=17
+		menuAukerapPal=17
 	fi
 
-done #Fin del bucle ppal
+done #ppal buklearen amaiera
 
-echo "Programa amaituta." #Fin del programa
-exit 0 #Fin del programa
+echo "Programa amaituta." 
+exit 0 #Programaren amaiera.
