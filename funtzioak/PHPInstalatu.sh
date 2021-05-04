fc8e1200d90cf95fa2b0a71f9f3913888596d760
Zuazo99 <gzuazo63@gmail.com>
2021-05-04 14:29:28 +0200

    editado lo que os he pasado al telegram

--- funtzioak/PHPInstalatu.sh
+++ funtzioak/PHPInstalatu.sh
@@ -6,8 +6,8 @@
 
 #source "$(dirname "$0")/funtzioak/instalatuta.sh"
 
-#function phpInstalatu()
-#{
+function phpInstalatu()
+{
 #echo "#################### phpInstalatu funtzioa"
 
 #instalatuta php #&> /dev/null
@@ -25,4 +25,52 @@
 #fi
 
 # service apache2 restart &>/dev/null 
-#}
+
+konprobaketa=`sudo aptitude show php | grep "Estado:"`
+	konprobaketa2=`echo $konprobaketa | grep "sin"`
+	if [ -n "$konprobaketa2" ]
+	then 
+		echo -e "\nphp paketea instalatuko da"
+		echo -e "Sakatu enter jarraitzeko"
+		read
+		sudo apt install php 
+		#sudo apt install php -y
+		konprobaketa=`sudo aptitude show php | grep "Estado:"`
+		konprobaketa2=`echo $konprobaketa | grep "instalado"`
+		
+		if [ -n "$konprobaketa2" ]
+		then
+			echo -e "\nphp paketea instalatu da"
+		else
+			echo -e "\nphp paketea ez da instalatu"
+		fi
+	else
+		echo -e "\nphp paketea instalatuta dago "
+	
+	fi
+	echo -e "Sakatu enter jarraitzeko eta libapache instalatzeko"
+	read
+	
+	#libapache2-mod-php paketea instalatu
+	konprobaketa=`sudo aptitude show libapache2-mod-php | grep "Estado:"`
+	konprobaketa2=`echo $konprobaketa | grep "sin"`
+	
+	if [ -n "$konprobaketa2" ]
+	then
+		echo -e "libapache2-mod-php paketea instalatuko da"
+		echo -e "Sakatu enter jarraitzeko"
+		read
+		sudo apt install libapache2-mod-php
+		konprobaketa=`sudo aptitude show libapache2-mod-php | grep "Estado:"`
+		konprobaketa2=`echo $konprobaketa | grep "instalado"`
+		if [ -n "$konprobaketa2" ]
+		then 
+			echo -e "\nlibapache instalatu egin da"
+		else
+			echo -e "\nlibapache ez da instalatu"
+		fi
+	else
+		echo -e "libapache2-mod-php jada instalatuta daukazu" 
+	
+	fi
+}
