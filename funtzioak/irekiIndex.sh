1a148bc76dbfa2a221427efc6341c47d966739d5
Duxon900 <quintanojon@gmail.com>
2021-05-04 19:38:52 +0200

    irekiIndex.sh

---
+++ funtzioak/irekiIndex.sh
@@ -0,0 +1,7 @@
+function irekiIndex(){
+	ipFiltratugabe=$(ip addr | grep -E "inet .*brd")
+	ipIaFiltratua=$(echo ${e:9:50})
+	ip=$(echo $ipIaFiltratua | cut -d"/" -f1)
+
+	firefox "$ip"/index.html
+}
