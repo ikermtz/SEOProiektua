2fc5d50413a82e7859f2b96708cb536736b50a19
Duxon900 <quintanojon@gmail.com>
2021-05-04 09:55:03 +0200

    apache 80 portua

---
+++ funtzioak/apache80PortuaBegiratu.sh
@@ -0,0 +1,15 @@
+function apache80PortuaBegiratu(){
+	begiratua=$(sudo netstat -anp | grep -E '80.*apache')
+
+	if [ -n begiratua ]
+	then
+		dialog --backtitle "Apache portuan"\
+	 	 --title "Apache portuan" \
+	 	 --msgbox "Apache 80 portura begira dago!" 10 20
+	else
+		dialog --backtitle "Apache ez dago"\
+	 	 --title "Apache ez dago" \
+	 	 --msgbox "Apache ez dago 80 portura begira!" 10 20
+	fi;
+
+}
