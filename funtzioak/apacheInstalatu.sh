86047009f43c3e83d19634e28d7ac25a381d55ef
olaba29 <andoni.ola@gmail.com>
2021-05-05 21:17:40 +0200

    PHP no va, pero quintano lo tenía peor

--- funtzioak/apacheInstalatu.sh
+++ funtzioak/apacheInstalatu.sh
@@ -1,43 +1,46 @@
-konprobatu(){
-	egoera=$(dpkg --get-selections | grep apache)
+#konprobatu(){
+#	egoera=$(dpkg --get-selections | grep apache)
+#
+#
+#
+#	if [ -n "$egoera" ]
+#	then #[["$egoera" == *"install"* ]]
+#		echo 0
+#	else 
+#		echo 1
+#	fi;
+#
+#}
 
-
-
-	if [ -n "$egoera" ]
-	then #[["$egoera" == *"install"* ]]
-		echo 0
-	else 
-		echo 1
-	fi;
-
-}
-
-apacheInstalatu(){
+function apacheInstalatu(){
 	dialog --backtitle "Apache Instalatu"\
 	 --title "Menu"\
 	 --msgbox "Apacheren egoera konprobatzen..." 10 50
 
 
-	echo "kaixo2"
-	ins=$(konprobatu)
-	echo "$ins"
+	#echo "kaixo2"
+	#ins=$(konprobatu)
+	#echo "$ins"
+	konprobatu =`dpkg --get-selections | grep '^apache*'`
 
-	if [ "$ins" == 0 ] 
+	if [ -n "$konprobatu" ] 
 	then
-		dialog --backtitle "Instalatuta"\
-		 --title "Bobobobo"\
-		 --msgbox "Apache instalatuta dago!" 10 50
-	else
-		dialog --backtitle "Apache Instalatu"\
+				dialog --backtitle "Apache Instalatu"\
 		 --title "Instalazioa"\
-		 --yesno "Apache ez dago instalatuta, instalatu nahi duzu?" 10 50
+		 --msgbox "Apache instalatuko da" 10 50
+		# --yesno "Apache ez dago instalatuta, instalatu nahi duzu?" 10 50
+		sudo apt install apache2 -y
+		echo -e "Hay algo"
+		#erantzuna=$?
+		#case $erantzuna in
+		#	0) sudo apt install apache2 -y;;
+		#	1) echo "bale crac";;
+		#	255) echo "pos vale sunormal";;
+		#esac
+	else
+		dialog --title "Instalatuta"\
+		 --msgbox "Apache instalatuta dago!" 10 50
 
-		erantzuna=$?
-		case $erantzuna in
-			0) sudo apt install apache2 -y;;
-			1) echo "bale crac";;
-			255) echo "pos vale sunormal";;
-		esac
-	fi;
+	fi
 
 }
