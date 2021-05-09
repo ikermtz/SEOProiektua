a67cad9658e1f684f7f24a697cecdf3618b9212b
Duxon900 <quintanojon@gmail.com>
2021-05-09 16:54:27 +0200

    a

--- funtzioak/irekiIndex.sh
+++ funtzioak/irekiIndex.sh
@@ -1,8 +1,6 @@
 function irekiIndex(){
 	ipFiltratugabe=$(ip addr | grep -E "inet .*brd")
-	echo $ipFiltratugabe
 	ipIaFiltratua=$(echo ${ipFiltratugabe:9:50})
-	echo $ipIaFiltratua
 	ip=$(echo $ipIaFiltratua | cut -d"/" -f1)
 	
 
