06078fdd8f77eb9f6658b972150f4df64bf39c8a
Duxon900 <quintanojon@gmail.com>
2021-05-11 00:21:24 +0200

    searx abiarazi badoa

--- funtzioak/searxAbiarazi.sh
+++ funtzioak/searxAbiarazi.sh
@@ -20,20 +20,25 @@ function searxAbiarazi ()
 	make install > /dev/null
 
 
-	python3 searx/setup.py install &
+	sudo python3 ./setup.py install > /dev/null &
 	ID=$!
 	wait "$ID"
-
-	python3 searx/webapp.py 
 
 	dialog --backtitle "Searx habiarazi"\
 		--title "Menu"\
 		--yesno "Searx martxan jarri da 127.0.0.1:8888 portuan. Ireki nahiko zenuke?" 10 50
 
+
 	response=$?
 	case $response in
-	   0) firefox 127.0.0.1:8888;;
+	   0) barruan;;
 	esac
 
 
+}
+
+function barruan(){
+
+	sudo python3 searx/webapp.py &> /dev/null &
+	firefox 127.0.0.1:8888
 }
